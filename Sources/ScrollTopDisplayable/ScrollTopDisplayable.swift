bc06c0a75a0759885e0904766d5c9540357e5e10
Oh Sangho <dhtkdgh429@gmail.com>
2023-03-01 05:02:20 +0900

    fix: lint

--- Sources/ScrollTopDisplayable/ScrollTopDisplayable.swift
+++ Sources/ScrollTopDisplayable/ScrollTopDisplayable.swift
@@ -7,7 +7,7 @@ private var sdScrollTopDisplayablePrevieousFrameY: CGFloat?
 private var sdScrollTopDisplayableLastContentOffsetY: CGFloat?
 
 public protocol ScrollTopDisplayable: UIView {
-	
+    
     var isVisible: Bool { get }
     
     var configHandler: ((_ viewHeight: CGFloat) -> Void)? { get set }
@@ -22,7 +22,7 @@ public protocol ScrollTopDisplayable: UIView {
     ///     }
     ///
     ///     - ViewController
-    ///     
+    ///
     ///     override func viewDidLoad() {
     ///         super.viewDidLoad()
     ///
@@ -36,10 +36,10 @@ public protocol ScrollTopDisplayable: UIView {
     ///     }
     func configure(frameY: CGFloat?, height: CGFloat?)
     
-	func didScroll(_ scrollView: UIScrollView)
+    func didScroll(_ scrollView: UIScrollView)
     
-	/// Use 'syncPreviousFrameY' function on `layoutSubviews`, if forced to update layouts.
-	func syncPreviousFrameY()
+    /// Use 'syncPreviousFrameY' function on `layoutSubviews`, if forced to update layouts.
+    func syncPreviousFrameY()
 }
 
 public extension ScrollTopDisplayable {
@@ -67,57 +67,57 @@ public extension ScrollTopDisplayable {
         configHandler?(viewHeight)
     }
     
-	func didScroll(_ scrollView: UIScrollView) {
-		let lastContentOffsetY: CGFloat = getAssociatedObject(self, &sdScrollTopDisplayableLastContentOffsetY) ?? 0
+    func didScroll(_ scrollView: UIScrollView) {
+        let lastContentOffsetY: CGFloat = getAssociatedObject(self, &sdScrollTopDisplayableLastContentOffsetY) ?? 0
         let viewHeight: CGFloat = getAssociatedObject(self, &sdScrollTopDisplayableViewHeight) ?? 0
         let startFrameY: CGFloat = getAssociatedObject(self, &sdScrollTopDisplayableStartFrameY) ?? 0
-		let previousFrameY: CGFloat = getAssociatedObject(self, &sdScrollTopDisplayablePrevieousFrameY) ?? startFrameY
-		let verticalDirection = scrollView.verticalDirection
-		let contentOffsetY = scrollView.contentOffset.y + viewHeight
-		let diffY = lastContentOffsetY - contentOffsetY
+        let previousFrameY: CGFloat = getAssociatedObject(self, &sdScrollTopDisplayablePrevieousFrameY) ?? startFrameY
+        let verticalDirection = scrollView.verticalDirection
+        let contentOffsetY = scrollView.contentOffset.y + viewHeight
+        let diffY = lastContentOffsetY - contentOffsetY
         
-		defer {
-			setRetainedAssociatedObject(self, &sdScrollTopDisplayableLastContentOffsetY, contentOffsetY)
-		}
-		
-		if diffY.isNaN {
-			return
-		}
-		
-		switch verticalDirection {
-		case .up:
-			guard contentOffsetY > 0 else { break }
-			
-			if frame.origin.y > previousFrameY {
-				frame.origin.y = previousFrameY
-			}
-			
-			frame.origin.y += diffY
-			
-			let topPinY = -abs(startFrameY - viewHeight)
-			if frame.origin.y <= topPinY {
-				frame.origin.y = topPinY
-			}
-			
-			setRetainedAssociatedObject(self, &sdScrollTopDisplayablePrevieousFrameY, frame.origin.y)
+        defer {
+            setRetainedAssociatedObject(self, &sdScrollTopDisplayableLastContentOffsetY, contentOffsetY)
+        }
+        
+        if diffY.isNaN {
+            return
+        }
+        
+        switch verticalDirection {
+        case .up:
+            guard contentOffsetY > 0 else { break }
             
-		case .down:
-			guard lastContentOffsetY >= contentOffsetY else { break }
-			
-			frame.origin.y += diffY
-			
-			if frame.origin.y >= startFrameY {
-				frame.origin.y = startFrameY
-			}
-			
-			setRetainedAssociatedObject(self, &sdScrollTopDisplayablePrevieousFrameY, frame.origin.y)
+            if frame.origin.y > previousFrameY {
+                frame.origin.y = previousFrameY
+            }
             
-		case .none:
-			break
-		}
-	}
-	
-	func syncPreviousFrameY() {
-		frame.origin.y = getAssociatedObject(self, &sdScrollTopDisplayablePrevieousFrameY) ?? 0
-	}
+            frame.origin.y += diffY
+            
+            let topPinY = -abs(startFrameY - viewHeight)
+            if frame.origin.y <= topPinY {
+                frame.origin.y = topPinY
+            }
+            
+            setRetainedAssociatedObject(self, &sdScrollTopDisplayablePrevieousFrameY, frame.origin.y)
+            
+        case .down:
+            guard lastContentOffsetY >= contentOffsetY else { break }
+            
+            frame.origin.y += diffY
+            
+            if frame.origin.y >= startFrameY {
+                frame.origin.y = startFrameY
+            }
+            
+            setRetainedAssociatedObject(self, &sdScrollTopDisplayablePrevieousFrameY, frame.origin.y)
+            
+        case .none:
+            break
+        }
+    }
+    
+    func syncPreviousFrameY() {
+        frame.origin.y = getAssociatedObject(self, &sdScrollTopDisplayablePrevieousFrameY) ?? 0
+    }
 }
--- Sources/ScrollTopDisplayable/UIScrollView+Extension.swift
+++ Sources/ScrollTopDisplayable/UIScrollView+Extension.swift
@@ -3,27 +3,27 @@ import UIKit
 private var sdVerticalDirectionLastOffsetYKey: CGFloat?
 
 extension UIScrollView {
-	
-	enum SDVerticalDirection {
-		
-		case none
-		case up
-		case down
-	}
-	
-	var verticalDirection: SDVerticalDirection {
-		let lastOffsetY: CGFloat = getAssociatedObject(self, &sdVerticalDirectionLastOffsetYKey) ?? 0
-		let contentOffsetY = contentOffset.y
-		
-		if lastOffsetY > contentOffsetY {
-			setRetainedAssociatedObject(self, &sdVerticalDirectionLastOffsetYKey, contentOffsetY)
-			return .down
-		} else if lastOffsetY < contentOffsetY {
-			setRetainedAssociatedObject(self, &sdVerticalDirectionLastOffsetYKey, contentOffsetY)
-			return .up
-		}
-		
-		setRetainedAssociatedObject(self, &sdVerticalDirectionLastOffsetYKey, contentOffsetY)
-		return .none
-	}
+    
+    enum SDVerticalDirection {
+        
+        case none
+        case up
+        case down
+    }
+    
+    var verticalDirection: SDVerticalDirection {
+        let lastOffsetY: CGFloat = getAssociatedObject(self, &sdVerticalDirectionLastOffsetYKey) ?? 0
+        let contentOffsetY = contentOffset.y
+        
+        if lastOffsetY > contentOffsetY {
+            setRetainedAssociatedObject(self, &sdVerticalDirectionLastOffsetYKey, contentOffsetY)
+            return .down
+        } else if lastOffsetY < contentOffsetY {
+            setRetainedAssociatedObject(self, &sdVerticalDirectionLastOffsetYKey, contentOffsetY)
+            return .up
+        }
+        
+        setRetainedAssociatedObject(self, &sdVerticalDirectionLastOffsetYKey, contentOffsetY)
+        return .none
+    }
 }
